759a9e84949387aae8dff3cfddb0dd40fb9c4d70
cagri3434 <caca_3460@hotmail.com>
2019-05-01 01:53:11 +0300

    Add files via upload

---
+++ komutlar/banmesaj.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+	if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('**Eğlence Komutları Özel Mesajlarda Kullanılamaz!**')
+    return message.author.sendEmbed(ozelmesajuyari); }
+    if (message.channel.type !== 'dm') {
+      const sunucubilgi = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setTimestamp()
+    .setDescription('')
+		.setImage(`https://66.media.tumblr.com/4ccb9751d6ff5ded860d06e184bddb45/tumblr_ov78drf3tf1vmjs6wo1_500.gif`)
+    return message.channel.sendEmbed(sunucubilgi);
+    }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yargı',
+  description: 'Dene ve Gör :D',
+  usage: 'yargı'
+};
